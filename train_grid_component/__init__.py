bb7b9374565902597664e8cb16cc734c86696a9e
Kacper <szmajdakacper@gmail.com>
2025-09-25 12:24:05 +0200

    feat: expose train_grid/train_plot in package __init__; include dist for Streamlit Cloud

--- train_grid_component/__init__.py
+++ train_grid_component/__init__.py
@@ -1,2 +1,9 @@
-# Marker package for train_grid_component
+"""Public API for the train_grid_component package.
 
+Allows convenient imports:
+    from train_grid_component import train_grid
+"""
+
+from .backend.train_grid_component import train_grid  # re-export
+
+__all__ = ["train_grid"]
--- train_plot_component/__init__.py
+++ train_plot_component/__init__.py
@@ -1,2 +1,10 @@
-# Marker package for train_plot_component
+"""Public API for the train_plot_component package.
+
+Allows convenient imports:
+    from train_plot_component import train_plot
+"""
+
+from .backend.train_plot_component import train_plot  # re-export
+
+__all__ = ["train_plot"]
 
